b7108515df9c331d7a9c5f937e29e43745b495e3
Ivan <gudleyd@gmail.com>
2020-04-19 15:14:10 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/RungeKutta.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RungeKutta.dir/main.cpp.obj"
+  "RungeKutta.exe"
+  "RungeKutta.exe.manifest"
+  "RungeKutta.pdb"
+  "libRungeKutta.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RungeKutta.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
