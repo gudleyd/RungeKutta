0482cdd0370b0f7eb704c02f74f938723d6a0c8c
Ivan <gudleyd@gmail.com>
2020-04-22 23:47:23 +0300

    Runge Kutta System Solver

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <chrono>
 
 #include "src/expression/Expression.h"
 #include "src/runge-kutta/RungeKutta.h"
 
 int main() {
     rk::Expression<float> p;
-    std::string s = "(x - y) / 2";
+    std::string s = "(x - y) / 2 + sin(x * pow(x, sin(y) * sin(y)))";
     p.parse(s, {"x", "y"}, utils_rk::stringToFloat);
-    std::cout << p.evaluate({2, 3}) << std::endl;
-    std::cout << rk::RK_solve_one<float>(p, {0, 1}, 2, 0.2) << std::endl;
+    p.compile();
+    std::cout << rk::RKSolve<float>(p, {0, 1}, 10, 0.0001)[1] << std::endl;
+    std::cout << rk::RKSolveSystem<float>({std::shared_ptr<rk::Expression<float>>(&p)}, {0, 1}, 10, 0.0001)[1] << std::endl;
     return 0;
 }
+
--- src/runge-kutta/RungeKutta.h
+++ src/runge-kutta/RungeKutta.h
@@ -13,12 +13,13 @@
 namespace rk {
 
     template<typename Value>
-    Value RK_solve_one(const Expression<Value>& function,
-                        std::vector<Value> initValues,
-                        Value at,
-                        Value h = 0.001) {
+    std::vector<Value> RKSolve(const Expression<Value>& function,
+                                std::vector<Value> initValues,
+                                Value at,
+                                Value h = 0.001) {
         auto n = (size_t)((at - initValues[0]) / h);
         Value k1, k2, k3, k4;
+        Value frac = (Value(1) / Value(6));
         for (size_t i = 1; i <= n; ++i) {
             k1 = h * function.evaluate(initValues);
             initValues[0] += 0.5 * h;
@@ -29,8 +30,39 @@ namespace rk {
             initValues[0] += 0.5 * h;
             initValues[1] += k3 - 0.5 * k2;
             k4 = h * function.evaluate(initValues);
-            initValues[1] += (Value(1) / Value(6)) * (k1 + 2 * k2 + 2 * k3 + k4) - k3;
+            initValues[1] += frac * (k1 + 2 * k2 + 2 * k3 + k4) - k3;
         }
-        return initValues[1];
+        return std::move(initValues);
+    }
+
+    template<typename Value>
+    std::vector<Value> RKSolveSystem(const std::vector<std::shared_ptr<Expression<Value>>>& functions,
+                                std::vector<Value> initValues,
+                                Value at,
+                                Value h = 0.001) {
+        auto n = (size_t)((at - initValues[0]) / h);
+        std::vector<std::vector<Value>> k(functions.size(), std::vector<Value>(4));
+        Value frac = (Value(1) / Value(6));
+        for (size_t i = 1; i <= n; ++i) {
+            for (size_t t = 0; t < functions.size(); ++t)
+                k[t][0] = h * functions[t]->evaluate(initValues);
+            initValues[0] += 0.5 * h;
+            for (size_t t = 0; t < functions.size(); ++t)
+                initValues[t + 1] += 0.5 * k[t][0];
+            for (size_t t = 0; t < functions.size(); ++t)
+                k[t][1] = h * functions[t]->evaluate(initValues);
+            for (size_t t = 0; t < functions.size(); ++t)
+                initValues[t + 1] += 0.5 * k[t][1] - 0.5 * k[t][0];
+            for (size_t t = 0; t < functions.size(); ++t)
+                k[t][2] = h * functions[t]->evaluate(initValues);
+            initValues[0] += 0.5 * h;
+            for (size_t t = 0; t < functions.size(); ++t)
+                initValues[t + 1] += k[t][2] - 0.5 * k[t][1];
+            for (size_t t = 0; t < functions.size(); ++t)
+                k[t][3] = h * functions[t]->evaluate(initValues);
+            for (size_t t = 0; t < functions.size(); ++t)
+                initValues[t + 1] += frac * (k[t][0] + 2 * k[t][1] + 2 * k[t][2] + k[t][3]) - k[t][2];
+        }
+        return std::move(initValues);
     }
 }
